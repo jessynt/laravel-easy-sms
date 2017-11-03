da11859130c6c7a93b57b7aba2949628b94a8282
xiao <xiao@comicool.cn>
2017-11-03 10:59:54 +0800

    Initial commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Jessynt\LaravelEasySMS;
+
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+use Illuminate\Foundation\Application as LaravelApplication;
+use Laravel\Lumen\Application as LumenApplication;
+use Overtrue\EasySms\EasySms;
+
+/**
+ * Class ServiceProvider
+ * @package Jessynt\LaravelEasySMS
+ */
+class ServiceProvider extends LaravelServiceProvider
+{
+    public function boot()
+    {
+        $this->setupConfig();
+    }
+
+    public function register()
+    {
+        $this->app->singleton('easysms', function (Container $app) {
+            return new EasySms(config('easysms'));
+        });
+
+        $this->app->alias('easysms', EasySms::class);
+    }
+
+    public function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/config.php');
+
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('easysms.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('easysms');
+        }
+
+        $this->mergeConfigFrom($source, 'easysms');
+    }
+}
